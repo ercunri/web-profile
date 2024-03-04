6faa23b0239b4b5c6338bc6d953dd56ad41fada7
mochammadfarkhan <55824712+mochammadfarkhan@users.noreply.github.com>
2024-03-04 20:28:12 +0700

    push anggota tim

--- src/Pages/Team.js
+++ src/Pages/Team.js
@@ -1,6 +1,10 @@
 // Team.js
 import logo from '../images/robot1.png'
 import erc from '../images/erc.png'
+import bertuah from '../images/bertuah1.jpg'
+import bambang from '../images/3.png'
+import dela from '../images/2.png'
+import labib from '../images/1.png'
 
 import React, { useState, useEffect } from 'react'
 
@@ -30,7 +34,7 @@ const Team = () => {
 
 
 
-            <div className='flex items-center justify-center gap-3 p-10 text-lg text-center '>
+            <div className='flex items-center justify-center gap-4 p-10 text-lg text-center '>
 
                 <div className="col-4">
                     <div className="mb-4">
@@ -38,13 +42,14 @@ const Team = () => {
                             onClick={(e) => {
                                 hideShowDiv(e)
                             }}
-                            className={isActive.id === 'divOne' ? `px-3 py-1 text-white bg-blue-600 rounded-md  ` : ' text-black'}
+                            className={isActive.id === 'divOne' ? `px-3 py-1 text-white bg-blue-600 rounded-md   ` : 'hover:text-blue-500 text-black'}
                         >
                             Core Team
                         </button>
                     </div>
 
                 </div>
+                
                 <div className="col-4">
                     <div className="mb-4">
                         <button
@@ -52,7 +57,60 @@ const Team = () => {
                             onClick={(e) => {
                                 hideShowDiv(e)
                             }}
-                            className={isActive.id === 'divTwo' ? `px-3 py-1 text-white bg-blue-600 rounded-md  ` : ' text-black'}
+                            className={isActive.id === 'divTwo' ? `px-3 py-1 text-white bg-blue-600 rounded-md   ` : 'hover:text-blue-500 text-black'}
+                        >
+                            Creative
+                        </button>
+                    </div>
+
+                </div>
+                <div className="col-4">
+                    <div className="mb-4">
+                        <button id="divThree"
+                            onClick={(e) => {
+                                hideShowDiv(e)
+                            }}
+                            className={isActive.id === 'divThree' ? `px-3 py-1 text-white bg-blue-600 rounded-md   ` : 'hover:text-blue-500 text-black'}
+                        >
+                            Asset Management
+                        </button>
+                    </div>
+
+                </div>
+                <div className="col-4">
+                    <div className="mb-4">
+                        <button id="divFour"
+                            onClick={(e) => {
+                                hideShowDiv(e)
+                            }}
+                            className={isActive.id === 'divFour' ? `px-3 py-1 text-white bg-blue-600 rounded-md   ` : 'hover:text-blue-500 text-black'}
+                        >
+                            Research
+                        </button>
+                    </div>
+
+                </div>
+                <div className="col-4">
+                    <div className="mb-4">
+                        <button id="divFive"
+                            onClick={(e) => {
+                                hideShowDiv(e)
+                            }}
+                            className={isActive.id === 'divFive' ? `px-3 py-1 text-white bg-blue-600 rounded-md   ` : 'hover:text-blue-500 text-black'}
+                        >
+                            Human Resource
+                        </button>
+                    </div>
+
+                </div>
+                <div className="col-4">
+                    <div className="mb-4">
+                        <button
+                            id="divSix"
+                            onClick={(e) => {
+                                hideShowDiv(e)
+                            }}
+                            className={isActive.id === 'divSix' ? `px-3 py-1 text-white bg-blue-600 rounded-md   ` : 'hover:text-blue-500 text-black'}
                         >
                             Competition Team
                         </button>
@@ -67,69 +125,6 @@ const Team = () => {
 
 
 
-
-                <div className="flex flex-col-reverse items-start justify-center gap-3 p-10 md:flex-row ">
-
-                    <div class="">
-                        <img src={logo} className="w-64 h-64" alt="logo" />
-
-                    </div>
-                    <div className="flex flex-col items-center gap-3 text-center md:w-2/3 md:items-start md:text-left">
-                        <p className='text-sm font-bold tracking-wider text-blue-500 uppercase'>Divisi ERC</p>
-                        <p className="text-5xl font-semibold text-blue-500">
-                            Divisi <span className='text-yellow-300'>Kreatif</span>
-                        </p>
-                        <ol className='flex flex-col w-3/4 gap-3 p-3 text-lg tracking-wider text-gray-500 list-decimal font-base'>
-                            <li>
-                                Mendokumentasi dan
-                                mempublikasikan kegiatan yang
-                                berkaitan dengan ERC
-                            </li>
-                            <li>
-                                Mendesain banner atau spanduk
-                                untuk kegiatan ERC
-                            </li>
-                            <li>
-                                Membuat konten 2 kali dalam 1
-                                bulan
-                            </li>
-                            <li>
-                                Mengelola media sosial ERC
-                            </li>
-                        </ol>
-
-                    </div>
-                </div>
-
-                <div className="flex flex-col-reverse items-start justify-center gap-3 p-10 md:flex-row ">
-
-                    <div class="">
-                        <img src={logo} className="w-64 h-64" alt="logo" />
-
-                    </div>
-                    <div className="flex flex-col items-center gap-3 text-center md:w-2/3 md:items-start md:text-left">
-                        <p className='text-sm font-bold tracking-wider text-blue-500 uppercase'>Divisi ERC</p>
-                        <p className="text-5xl font-semibold text-blue-500">
-                            Divisi <span className='text-yellow-300'>Asset </span>
-                            <span className='text-red-500'>Management</span>
-                        </p>
-                        <ol className='flex flex-col w-3/4 gap-3 p-3 text-lg tracking-wider text-gray-500 list-decimal font-base'>
-                            <li>
-                                Menjaga aset dalam secretariat ERC
-                            </li>
-                            <li>Mencatat nama, jumlah, kondisi Aset
-                                yang dimiliki ERC
-                            </li>
-                            <li>Menjaga kebersihan, ketentraman,
-                                kenyamanan di dalam secretariat
-                                ERC
-                            </li>
-
-
-                        </ol>
-
-                    </div>
-                </div>
                 <div className="flex flex-col-reverse items-start justify-center gap-3 p-10 md:flex-row ">
 
                     <div class="">
@@ -193,19 +188,120 @@ const Team = () => {
 
 
             </div>
-
             <div className={isActive.id === 'divTwo' ? `divTwo` : 'divTwo hidden'}>
-                <div className="flex flex-col items-center gap-3 pb-10">
-                    <p className='pb-10 text-3xl font-semibold'>
-                        Tim KRSBI Bertuah (2024)
+                <div className="flex flex-col-reverse items-start justify-center gap-3 p-10 md:flex-row ">
+
+                    <div class="">
+                        <img src={logo} className="w-64 h-64" alt="logo" />
+
+                    </div>
+                    <div className="flex flex-col items-center gap-3 text-center md:w-2/3 md:items-start md:text-left">
+                        <p className='text-sm font-bold tracking-wider text-blue-500 uppercase'>Divisi ERC</p>
+                        <p className="text-5xl font-semibold text-blue-500">
+                            Divisi <span className='text-yellow-300'>Kreatif</span>
+                        </p>
+                        <ol className='flex flex-col w-3/4 gap-3 p-3 text-lg tracking-wider text-gray-500 list-decimal font-base'>
+                            <li>
+                                Mendokumentasi dan
+                                mempublikasikan kegiatan yang
+                                berkaitan dengan ERC
+                            </li>
+                            <li>
+                                Mendesain banner atau spanduk
+                                untuk kegiatan ERC
+                            </li>
+                            <li>
+                                Membuat konten 2 kali dalam 1
+                                bulan
+                            </li>
+                            <li>
+                                Mengelola media sosial ERC
+                            </li>
+                        </ol>
+
+                    </div>
+                </div>
+            </div>
+
+            <div className={isActive.id === 'divThree' ? `divThree` : 'divThree hidden'}>
+
+                <div className="flex flex-col-reverse items-start justify-center gap-3 p-10 md:flex-row ">
+
+                    <div class="">
+                        <img src={logo} className="w-64 h-64" alt="logo" />
+
+                    </div>
+                    <div className="flex flex-col items-center gap-3 text-center md:w-2/3 md:items-start md:text-left">
+                        <p className='text-sm font-bold tracking-wider text-blue-500 uppercase'>Divisi ERC</p>
+                        <p className="text-5xl font-semibold text-blue-500">
+                            Divisi <span className='text-yellow-300'>Asset </span>
+                            <span className='text-red-500'>Management</span>
+                        </p>
+                        <ol className='flex flex-col w-3/4 gap-3 p-3 text-lg tracking-wider text-gray-500 list-decimal font-base'>
+                            <li>
+                                Menjaga aset dalam secretariat ERC
+                            </li>
+                            <li>Mencatat nama, jumlah, kondisi Aset
+                                yang dimiliki ERC
+                            </li>
+                            <li>Menjaga kebersihan, ketentraman,
+                                kenyamanan di dalam secretariat
+                                ERC
+                            </li>
+
+
+                        </ol>
+
+                    </div>
+                </div>
+            </div>
+
+            <div className={isActive.id === 'divSix' ? `divSix` : 'divSix hidden'}>
+                <div className="flex flex-col items-center gap-3 pt-3 pb-10">
+                    <p className="text-2xl font-semibold text-blue-500">
+                        Tim <span className='text-yellow-300'>KRSBI </span>
+                        <span className='text-red-500'>2024</span>
                     </p>
-                    <div className='grid grid-cols-1 gap-6 font-semibold text-black md:grid-cols-3'>
-                        <div className='w-48 h-64 p-3 border-2 border-blue-600 rounded-lg'>Bambang Trimukti</div>
-                        <div className='w-48 h-64 p-3 border-2 border-blue-600 rounded-lg'>Farhan</div>
-                        <div className='w-48 h-64 p-3 border-2 border-blue-600 rounded-lg'>abdul aziz</div>
-                        <div className='w-48 h-64 p-3 border-2 border-blue-600 rounded-lg'>Abul</div>
-                        <div className='w-48 h-64 p-3 border-2 border-blue-600 rounded-lg'>Fikri</div>
-                        <div className='w-48 h-64 p-3 border-2 border-blue-600 rounded-lg'>Dela</div>
+                    <p className='text-5xl font-semibold text-blue-500'>"Bertuah"</p>
+
+                    <div className='grid grid-cols-1 gap-6 m-2 font-semibold text-black md:grid-cols-3'>
+                        <div className='relative w-48 h-48 p-3 bg-white bg-center bg-no-repeat bg-contain border-8 border-white rounded-full shadow-md' style={{ backgroundImage: `url(${bambang})` }}>
+                            <div className='absolute px-4 py-2 text-white bg-blue-600 rounded-md shadow-md -bottom-4'>
+                                <p className='self-end text-sm'>Bambang Tri Mukti</p>
+                                <p className='self-end text-xs font-light'>Team leader/Electrical</p>
+                            </div>
+                        </div>
+                        <div className='relative w-48 h-48 p-3 bg-white bg-right-top bg-cover border-8 border-white rounded-full shadow-md' style={{ backgroundImage: `url(${labib})` }}>
+                            <div className='absolute px-4 py-2 text-white bg-blue-600 rounded-md shadow-md -bottom-4'>
+                                <p className='self-end text-sm'>Mohd. Labib</p>
+                                <p className='self-end text-xs font-light'> Programmer</p>
+                            </div>
+                        </div>
+                        <div className='relative w-48 h-48 p-3 bg-white bg-right-top bg-cover border-8 border-white rounded-full shadow-md' style={{ backgroundImage: `url(${dela})` }}>
+                            <div className='absolute px-4 py-2 text-white bg-blue-600 rounded-md shadow-md -bottom-4'>
+                                <p className='self-end text-sm'>Viera Adella</p>
+                                <p className='self-end text-xs font-light'> Programmer</p>
+                            </div>
+                        </div>
+                        <div className='relative w-48 h-48 p-3 bg-white bg-right-top bg-cover border-8 border-white rounded-full shadow-md' style={{ backgroundImage: `url(${dela})` }}>
+                            <div className='absolute px-4 py-2 text-white bg-blue-600 rounded-md shadow-md -bottom-4'>
+                                <p className='self-end text-sm'>Fikri Rivandi</p>
+                                <p className='self-end text-xs font-light'> Programmer</p>
+                            </div>
+                        </div>
+                        <div className='relative w-48 h-48 p-3 bg-white bg-right-top bg-cover border-8 border-white rounded-full shadow-md' style={{ backgroundImage: `url(${bambang})` }}>
+                            <div className='absolute px-4 py-2 text-white bg-blue-600 rounded-md shadow-md -bottom-4'>
+                                <p className='self-end text-sm'>M. Farhan</p>
+                                <p className='self-end text-xs font-light'> Programmer</p>
+                            </div>
+                        </div>
+                        <div className='relative w-48 h-48 p-3 bg-white bg-right-top bg-cover border-8 border-white rounded-full shadow-md' style={{ backgroundImage: `url(${labib})` }}>
+                            <div className='absolute px-4 py-2 text-white bg-blue-600 rounded-md shadow-md -bottom-4'>
+                                <p className='self-end text-sm'>Abdul Aziz</p>
+                                <p className='self-end text-xs font-light'>Electrical</p>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
